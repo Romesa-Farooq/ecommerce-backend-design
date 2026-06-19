1c73b90e4d8b18955aee74344cfabb4077b014e6
Romesa-Farooq <romesafarooq5@gmail.com>
2026-06-19 17:56:07 +0500

    Improved ecommerce UI

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
